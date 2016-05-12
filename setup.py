298ca4a4cca4a6366abc62e8e029981e85104ff9
Matt Oswalt <moswalt@ebay.com>
2016-05-12 12:47:33 -0700

    Added new setup.cfg and setup.py
    Signed-off-by: Matt Oswalt <moswalt@ebay.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
-"""setup.py file."""
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import uuid
 
-from setuptools import setup, find_packages
 from pip.req import parse_requirements
+from setuptools import find_packages
 
 from setuptools import find_packages
 from setuptools import setup
@@ -13,7 +27,19 @@ __author__ = 'David Barroso <dbarrosop@dravetech.com>'
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
 
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
+
 setup(
+    setup_requires=['pbr>=1.8'],
+    pbr=True,
     name="napalm-junos",
     version="0.2.1",
     packages=find_packages(),
@@ -29,5 +55,5 @@ setup(
     ],
     url="https://github.com/napalm-automation/napalm-junos",
     include_package_data=True,
-    install_requires=reqs,
+    install_requires=reqs
 )
